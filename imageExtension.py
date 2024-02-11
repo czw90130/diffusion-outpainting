8eb9eebc7f9ff6c032738a9328282d6070b5f824
ZhiweiCHU <czw90130@gmail.com>
2024-02-12 07:01:47 +0800

    note: 添加了详细注释

--- imageExtension.py
+++ imageExtension.py
@@ -1,8 +1,30 @@
+# 导入必要的库
+# UNet2DConditionModel
+# 这是一个条件UNet模型，用于生成或编辑图像
+# 在Stable Diffusion中，UNet是负责图像生成的核心网络，可以根据给定的条件（如文本描述）生成相应的图像
+# 它通过接收嵌入文本的提示和可能的其他输入（如遮罩区域的图像），来生成或编辑图像。
 from diffusers import UNet2DConditionModel
+# AutoencoderKL
+# 这是一个变分自编码器（VAE），在Stable Diffusion中用于图像的编码和解码。
+# 这个模型可以将图像编码到一个低维的隐空间表示中，并能从这个隐空间表示中重构图像。
+# 在图像编辑任务中，VAE用于将输入图像编码到隐空间，进行处理后再解码回图像空间。
 from diffusers import AutoencoderKL
+# DDIMScheduler
+# 这是一个确定性的差分图像生成（DDIM）调度器，用于控制图像生成过程中的噪声减少步骤。
+# 调度器负责在图像生成过程中安排时间步骤，以便逐步从噪声图像过渡到清晰图像。
+# 这种方法有助于生成高质量的图像，并可以调整以生成多样化的结果。
 from diffusers import DDIMScheduler
+# VaeImageProcessor
+# 这是一个图像处理器，专为与VAE模型一起使用而设计。
+# 它提供了一系列的预处理和后处理功能，使得图像能够被模型正确处理。
+# 预处理功能包括调整图像大小、标准化等，而后处理功能则负责将模型输出转换回可视化图像。
 from diffusers.image_processor import VaeImageProcessor
+# CLIPTokenizer & CLIPTextModel
+# 这两个来自transformers库的组件与diffusers库紧密协作，用于处理文本输入。
+# CLIPTokenizer用于将文本提示分词并编码为模型可以理解的格式，而CLIPTextModel则根据这些编码生成文本的嵌入表示。
+# 这些嵌入表示随后被用作生成或编辑图像时的条件输入。
 from transformers import CLIPTokenizer, CLIPTextModel
+
 import torch
 from tqdm import tqdm
 import cv2 as cv
@@ -11,85 +33,166 @@ import time
 from safetensors.torch import load_file
 from typing import List
 
-
-
 class ImageExtension():
+    """
+    图像扩展类，用于处理图像生成和编辑任务，特别是使用Stable Diffusion模型进行图像的生成、编辑和修复。
+    """
+    # 设置默认模型路径和本地缓存路径
     DEFAULT_MODEL = "stabilityai/stable-diffusion-2-inpainting"
     cache_dir = "F:/huggingface_model/"    # diffusers的本地缓存路径
     def __init__(self, 
                  sampleTimeStep:int = 30, 
                  only_local_files:bool = False):
-        self.base_model_name = self.DEFAULT_MODEL
-        self.only_local_files = only_local_files
-        self.sampleTimeStep = sampleTimeStep
-        self.load_model()
-        self.getLatent_model()
-        self.load_text_encoder()
-        self.load_scheduler()
-        
-        self.allTimestep = self.ddim.timesteps
+        """
+        初始化ImageExtension类的实例。
 
-        self.image_processor = VaeImageProcessor()
+        参数:
+        - sampleTimeStep: int, 定义采样过程中使用的时间步长，默认为30。
+        - only_local_files: bool, 指定是否仅使用本地文件加载模型，以避免从网络下载，默认为False。
+        """
+        # 初始化模型参数
+        self.base_model_name = self.DEFAULT_MODEL # 指定使用的模型
+        self.only_local_files = only_local_files # 设置是否仅使用本地文件
+        set_step(sampleTimeStep) # 设置采样时间步长
+
+        # 加载必要的模型和组件
+        self.load_model()  # 加载条件UNet模型
+        self.getLatent_model()  # 加载VAE模型，用于图像的编码和解码
+        self.load_text_encoder()  # 加载文本编码器，用于处理文本提示
+        self.load_scheduler()  # 加载调度器，用于控制图像生成过程中的时间步
+        
+        self.allTimestep = self.ddim.timesteps  # 获取调度器定义的所有时间步
+
+        self.image_processor = VaeImageProcessor()  # 初始化图像处理器，用于预处理和后处理图像
 
 
     def addNoise(self, latent:torch.Tensor, noise: torch.Tensor, timestep:torch.Tensor):
+        """
+        向隐空间表示添加噪声。这一操作是图像生成过程的关键步骤之一，有助于引入随机性。
+
+        参数:
+        - latent: torch.Tensor, 当前的隐空间表示，它是图像在高维空间的表示形式。
+        - noise: torch.Tensor, 要添加到隐空间表示中的噪声。这个噪声通常是随机生成的，有助于在生成过程中引入变异。
+        - timestep: torch.Tensor, 当前的时间步。在随时间步进行的图像生成过程中，不同的时间步可能需要添加不同程度的噪声。
+
+        返回:
+        - torch.Tensor: 添加噪声后的新的隐空间表示。
+        """
+        # 使用DDIM调度器的add_noise方法将噪声添加到隐空间表示中。
+        # 这个方法根据当前时间步计算噪声的加权，确保噪声的添加与生成过程的阶段相匹配。
         latent = self.ddim.add_noise(latent, noise, timestep)
+        # 对加入噪声后的隐空间表示进行缩放，使用init_noise_sigma进行调整。
+        # init_noise_sigma是一个预先定义的缩放因子，它根据模型的配置和训练过程中使用的噪声水平来设定。
         latent = latent * self.ddim.init_noise_sigma
     
         return latent
     
     def set_step(self, sampleTimeStep:int):
+        # 设置采样时间步
         self.sampleTimeStep = sampleTimeStep
         self.ddim.set_timesteps(self.sampleTimeStep, device="cuda:0")
         self.allTimestep = self.ddim.timesteps
     
         
     def sample_step(self, latent: torch.Tensor, niose: torch.Tensor, timestep: torch.Tensor):
+        """
+        进行单步采样，根据当前的隐空间表示、噪声和时间步更新隐空间表示。
+
+        参数:
+        - latent: torch.Tensor，当前的隐空间表示。
+        - noise: torch.Tensor，当前步骤要添加的噪声。
+        - timestep: torch.Tensor，当前的时间步。
+
+        返回:
+        - torch.Tensor: 更新后的隐空间表示。
+        """
+        # 使用DDIM调度器的step方法更新隐空间表示
         return self.ddim.step(niose, timestep, latent)['prev_sample']
 
 
     def sample_block(self, latent:torch.Tensor, masked_latent:torch.Tensor, mask: torch.Tensor, prompt_embeds:torch.Tensor, timestep: torch.Tensor, guidance_scale:int=7):
+        """
+        对指定区块进行采样，根据给定的文本提示通过模型生成对应的噪声，然后应用噪声生成最终的图像。
+
+        参数:
+        - latent: torch.Tensor，原始图像的隐空间表示。
+        - masked_latent: torch.Tensor，被遮罩的区域的隐空间表示。
+        - mask: torch.Tensor，遮罩，用于指定需要生成内容的区域。
+        - prompt_embeds: torch.Tensor，文本提示的嵌入表示。
+        - timestep: torch.Tensor，当前的时间步。
+        - guidance_scale: int，指导比例，用于调整条件生成和无条件生成的平衡。
+
+        返回:
+        - torch.Tensor: 更新后的隐空间表示。
+        """
+        # 准备输入数据：复制latent、mask和masked_latent以适应UNet的输入要求
         latent_model_input = torch.cat([latent] * 2)
         mask_input = torch.cat([mask]*2)
         masked_latent_input = torch.cat([masked_latent]*2)
 
+        # 调整输入数据的尺寸并合并，以符合模型的输入格式
         latent_model_input = self.ddim.scale_model_input(latent_model_input, timestep)
         latent_model_input = torch.cat([latent_model_input, mask_input, masked_latent_input], dim=1)     # inpaint模型拥有额外的输入信息，通道数为9
         
+        # 通过UNet模型预测噪声，并根据指导比例调整
         noise_pred = self.unet(latent_model_input, timestep, encoder_hidden_states = prompt_embeds).sample
         noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
         noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
 
+        # 使用预测的噪声更新隐空间表示
         latent = self.sample_step(latent, noise_pred, timestep)
 
         return latent
     
     def sample(self, latent:torch.Tensor, masked_latent:torch.Tensor, mask: torch.Tensor, chunk_list: List[int], prompt_embeds:torch.Tensor, guidance_scale:int=7):
+        """
+        对整个图像进行分块采样，遍历所有时间步，对每个块进行采样，最后合并结果。
+
+        参数:
+        - latent: torch.Tensor，原始图像的隐空间表示。
+        - masked_latent: torch.Tensor，被遮罩区域的隐空间表示。
+        - mask: torch.Tensor，遮罩，用于指定需要生成内容的区域。
+        - chunk_list: List[int]，需要采样的图像块的列表。
+        - prompt_embeds: torch.Tensor，文本提示的嵌入表示。
+        - guidance_scale: int，指导比例，用于调整条件生成和无条件生成的平衡。
+
+        返回:
+        - torch.Tensor: 更新后的整个图像的隐空间表示。
+        """
         # print(prompt_embeds.shape)
         # print(latent.shape)
-        count = torch.zeros_like(latent)
-        full_latent = torch.zeros_like(latent)
+        count = torch.zeros_like(latent) # 初始化计数器，用于记录每个像素被更新的次数
+        full_latent = torch.zeros_like(latent) # 初始化用于累加更新结果的张量
+
+        # 遍历所有时间步
         for Tin in tqdm(range(0, len(self.allTimestep))):
-            Ti = self.allTimestep[Tin]
-            count.zero_()
-            full_latent.zero_()
+            Ti = self.allTimestep[Tin]  # 获取当前时间步
+            count.zero_()  # 重置计数器
+            full_latent.zero_()  # 重置累加器
+            
+            # 遍历所有需要处理的图像块
             for chunk_block in chunk_list:
 
+                # 提取当前块的隐空间表示和遮罩
                 sample_latent = latent[:, :, chunk_block[0]:chunk_block[1], chunk_block[2]:chunk_block[3]]
                 sample_mask = mask[:, :, chunk_block[0]:chunk_block[1], chunk_block[2]:chunk_block[3]]
                 sample_masked_latent = masked_latent[:, :, chunk_block[0]:chunk_block[1], chunk_block[2]:chunk_block[3]]
 
+                # 对当前块进行采样
                 pred_noise = self.sample_block(sample_latent, sample_masked_latent, sample_mask, prompt_embeds, Ti, guidance_scale)   # 每一个时间步的采样过程
                 
+                # 累加更新结果，并更新计数器
                 full_latent[:, :, chunk_block[0]:chunk_block[1], chunk_block[2]:chunk_block[3]] += pred_noise
                 count[:, :, chunk_block[0]:chunk_block[1], chunk_block[2]:chunk_block[3]] += 1
 
-
+            # 使用累加结果和计数器更新最终的隐空间表示
             latent = torch.where(count > 0, full_latent / count, full_latent)
 
         return latent
     
     def load_scheduler(self):
+        # 加载调度器
+        # 加载DDIMScheduler用于控制图像生成过程中的噪声减少步骤
         MN = self.base_model_name
         self.ddim = DDIMScheduler.from_pretrained(MN, 
                                                   subfolder="scheduler", 
@@ -102,6 +205,8 @@ class ImageExtension():
         self.ddim.set_timesteps(self.sampleTimeStep, device="cuda:0")
 
     def load_model(self):
+        # 加载UNet模型
+        # 加载条件UNet模型用于图像生成
         self.unet = UNet2DConditionModel.from_pretrained(self.base_model_name, 
                                                             local_files_only = self.only_local_files, 
                                                             torch_dtype=torch.float16, 
@@ -113,6 +218,8 @@ class ImageExtension():
         self.unet.enable_xformers_memory_efficient_attention()
         
     def getLatent_model(self):
+        # 加载VAE模型
+        # 加载VAE模型用于编码和解码图像到隐空间
         MN = self.base_model_name
         self.vae = AutoencoderKL.from_pretrained(MN, 
                                                  local_files_only = self.only_local_files,
@@ -123,6 +230,8 @@ class ImageExtension():
         
 
     def load_text_encoder(self):
+        # 加载文本编码器
+        # 加载CLIP的文本模型和分词器，用于将文本转换为嵌入向量
         MN = self.base_model_name
         self.text_encoder = CLIPTextModel.from_pretrained(MN, 
                                                           local_files_only = self.only_local_files,
@@ -139,6 +248,8 @@ class ImageExtension():
         
     @staticmethod
     def tokenize_prompt(tokenizer, prompt):
+        # 分词处理文本提示
+        # 使用CLIPTokenizer处理文本提示
         text_inputs = tokenizer(
             prompt,
             padding="max_length",
@@ -151,6 +262,8 @@ class ImageExtension():
 
     
     def encode_prompt(self, prompt:str, neg_prompt:str = None):
+        # 编码文本提示
+        # 将正面和负面提示编码为嵌入向量
         text_input_ids = self.tokenize_prompt(self.tokenizer, prompt)
 
         prompt_embeds = self.text_encoder(
@@ -175,14 +288,20 @@ class ImageExtension():
         return prompt_embeds
 
     def get_text_embedding(self, prompt:str, neg_prompt:str = None):
+        # 获取文本嵌入向量
+        # 对外提供的接口，内部调用encode_prompt方法
         return self.encode_prompt(prompt, neg_prompt)
 
         
     def getImgLatent(self, img:torch.Tensor):
+        # 将图像编码到隐空间
+        # 使用VAE模型编码图像
         # img = self.image_processor.preprocess(img)
         return self.vae.encode(img).latent_dist.sample() * self.vae.config.scaling_factor
     
     def getImg(self, latent:torch.Tensor):
+        # 从隐空间解码图像
+        # 使用VAE模型解码隐向量到图像
         image = self.vae.decode(latent / self.vae.config.scaling_factor)[0]
         image = image.detach()
         image = self.image_processor.postprocess(image, output_type="np", do_denormalize=[True])
--- webUI.py
+++ webUI.py
@@ -21,26 +21,36 @@ class Event():
         return img, selection
     
     def render_box(self, img:np.ndarray):
+        # 获取输入图像的形状
         imgShape = img.shape
-        selection = [([max(-self.col_move, 0) * self.grid_size, 
-                       max(-self.row_move, 0) * self.grid_size, 
-                       max(-self.col_move, 0) * self.grid_size + imgShape[1], 
-                       max(-self.row_move, 0) * self.grid_size + imgShape[0]], "image")]
+
+        # 初始化selection列表，用于存储原始图像区域和扩展区域的坐标及类型
+        selection = [([max(-self.col_move, 0) * self.grid_size, # 计算原始图像左上角的x坐标
+                       max(-self.row_move, 0) * self.grid_size, # 计算原始图像左上角的y坐标
+                       max(-self.col_move, 0) * self.grid_size + imgShape[1], # 计算原始图像右下角的x坐标
+                       max(-self.row_move, 0) * self.grid_size + imgShape[0]], "image")] # 计算原始图像右下角的y坐标
+        
+        # 如果图像需要在任一方向上进行移动，则计算扩展区域的坐标，并添加到selection中
         if self.col_move != 0 or self. row_move != 0:
             selection.append(
-                     ([max(self.col_move, 0) * self.grid_size, 
-                       max(self.row_move, 0) * self.grid_size, 
-                       max(self.col_move, 0) * self.grid_size + imgShape[1], 
-                       max(self.row_move, 0) * self.grid_size + imgShape[0]], "extension")
+                     ([max(self.col_move, 0) * self.grid_size, # 计算扩展区域左上角的x坐标
+                       max(self.row_move, 0) * self.grid_size, # 计算扩展区域左上角的y坐标
+                       max(self.col_move, 0) * self.grid_size + imgShape[1], # 计算扩展区域右下角的x坐标
+                       max(self.row_move, 0) * self.grid_size + imgShape[0]], "extension") # 计算扩展区域右下角的y坐标
             )
 
+        # 根据移动的行列数计算新图像的尺寸
         newImgShape = [
             abs(self.row_move) * self.grid_size + imgShape[0],
             abs(self.col_move) * self.grid_size + imgShape[1]
         ]
+        # 创建一个新的空白图像
         newImg = np.zeros([*newImgShape, 3], dtype=np.uint8)
+
+        # 在新图像上复制原始图像到指定的位置
         newImg[selection[0][0][1] : selection[0][0][3], selection[0][0][0] : selection[0][0][2]] = img
 
+        # 返回新图像和边界框信息
         return (newImg, selection)
     
     def left_ext(self, img:np.ndarray):
@@ -76,54 +86,88 @@ class Event():
         return self.render_box(img)
     
     def process(self, img:np.ndarray, prompt:str, neg_prompt:str, sampleStep:int, guidance_scale:int):
+        """
+        处理图像，根据给定的文本提示进行图像生成或编辑。
+
+        参数:
+        - img: np.ndarray, 原始图像数组。
+        - prompt: str, 正面文本提示，描述希望生成或编辑到图像中的内容。
+        - neg_prompt: str, 负面文本提示，描述不希望出现在图像中的内容。
+        - sampleStep: int, 采样步骤的数量，影响生成过程的细节程度。
+        - guidance_scale: int, 指导缩放比例，控制正面提示与负面提示的影响力度。
+
+        返回:
+        - out_img: 处理后的图像数组。
+        """
+        # 调用render_box函数获取预处理后的图像和边界框信息
         newimg, bbox = self.render_box(img)
+        
+        # 清除CUDA缓存，以避免显存溢出
         torch.cuda.empty_cache()
 
+        # 设置采样步骤
         self.img_ext.set_step(sampleStep)
         with torch.no_grad():
-
+            # 将输入图像从NumPy数组转换为PyTorch张量，并调整为模型所需的形状和类型
             torchImg = torch.from_numpy(newimg).permute(2, 0, 1).unsqueeze(0).half()
+            # 根据边界框信息提取需要处理的图像区域，并进行归一化处理
             torchUseImg = torchImg[:, :, bbox[0][0][1] : bbox[0][0][3], bbox[0][0][0] : bbox[0][0][2]] / 127.5 - 1.0
 
+            # 将处理后的区域放回原图像对应位置
             torchImg[:, :, bbox[0][0][1] : bbox[0][0][3], bbox[0][0][0] : bbox[0][0][2]] = torchUseImg
 
+            # 将图像数据转移到CUDA设备上
             torchImg = torchImg.cuda().half()
+            # 创建一个遮罩，初始化为全0，然后根据bbox设置需要编辑的区域为1
             mask = np.zeros([torchImg.shape[2], torchImg.shape[3]], dtype=np.uint8)
             mask[bbox[1][0][1] : bbox[1][0][3], bbox[1][0][0] : bbox[1][0][2]] = 1      # 需要扩展的地方设置为mask 1
             mask[bbox[0][0][1] : bbox[0][0][3], bbox[0][0][0] : bbox[0][0][2]] = 0       # 重叠区域再次置零
 
+            # 使用中值滤波平滑遮罩边缘
             mask = cv.medianBlur(mask, 3)
             mask = mask[None, None, :, :]
+            # 将遮罩转换为torch张量，并传输到CUDA设备
             mask = torch.from_numpy(mask).cuda().half()
 
+            # 应用遮罩生成被遮挡的图像版本
             masked_img = torchImg * (mask < 0.5)  # 反向掩码
+
+            # 将原图和被遮挡的图像转换为隐空间表示
             latent = self.img_ext.getImgLatent(torchImg)
             masked_latent = self.img_ext.getImgLatent(masked_img)
 
+            # 将遮罩大小调整至隐空间的尺寸
             mask = torch.nn.functional.interpolate(
-                mask, size=(mask.shape[2] // 8, mask.shape[3] // 8)                            # mask 转换为latent尺寸
+                mask, size=(mask.shape[2] // 8, mask.shape[3] // 8) # mask 转换为latent尺寸
             )
 
+            # 为隐空间表示添加噪声
             noise = torch.randn_like(latent)
             extend_latent = self.img_ext.addNoise(latent, noise, self.img_ext.allTimestep[0])  # 加噪
 
+            # 计算处理块的列表，这些块基于bbox和缩放，转换为latent尺寸
             chunkList = [
                 [bbox[0][0][1]//8, bbox[0][0][3]//8, bbox[0][0][0]//8, bbox[0][0][2]//8], 
                 [bbox[1][0][1]//8, bbox[1][0][3]//8, bbox[1][0][0]//8, bbox[1][0][2]//8]
                 ]
 
-
+            # 获取正面和负面文本提示的嵌入表示
             prompt_embeds = self.img_ext.get_text_embedding(prompt, neg_prompt)
         
+            # 使用嵌入表示、隐空间表示和遮罩进行图像采样
             result = self.img_ext.sample(extend_latent, masked_latent, mask, chunkList, prompt_embeds, guidance_scale=guidance_scale)    
+            # 将隐空间表示解码回图像
             out_img = self.img_ext.getImg(result)[0]
 
+        # 调整输出图像大小以匹配原始图像
         out_img = cv.resize(out_img, (newimg.shape[1], newimg.shape[0]))
 
+        # 创建输出遮罩以确定哪些部分应从原始图像中保留
         outPaintMask = np.zeros_like(out_img, dtype=np.uint8)
         outPaintMask[bbox[1][0][1] : bbox[1][0][3], bbox[1][0][0] : bbox[1][0][2]] = 1   
         outPaintMask[bbox[0][0][1] : bbox[0][0][3], bbox[0][0][0] : bbox[0][0][2]] = 1
 
+        # 结合原图和生成的图像部分
         out_img = np.where(outPaintMask < 0.5, newimg, out_img)   
 
         return out_img
@@ -139,43 +183,74 @@ class Event():
 
 
 def setupUI(event_process:Event):
+    """
+    使用Gradio库设置用户界面，允许用户上传图片、输入文本提示、调整参数，并通过事件处理函数处理图像。
+
+    参数:
+    - event_process: Event 类的实例，包含了处理各种UI事件的函数。
+
+    返回:
+    - UI: Gradio界面实例。
+    """
     with gr.Blocks() as UI:
         with gr.Row():
+            # 创建一行用于放置图像上传和编辑区域
             with gr.Column():
-                image = gr.Image(height=300)
-                img_window = gr.AnnotatedImage(
+                # 创建一列用于放置图像上传组件
+                image = gr.Image(height=300) # 图像上传组件
+                img_window = gr.AnnotatedImage( # 图像编辑组件，用于显示和标记图像的不同区域
                     color_map={"image": "#a89a00", "extension": "#00aeff"},
                     height = 300
                 )
+                # 设置图像上传时的回调函数，上传图像后更新img_window组件
                 image.upload(event_process.image_change, inputs=[image], outputs=[img_window])
+                # 设置应用按钮点击时清空image组件的内容
                 image.clear(event_process.apply_to, inputs=[image])
+            # 预览处理后的图像
             pre_image = gr.Image(height=600, interactive=False)
 
+        # 创建另一行用于放置文本输入框、滑块和按钮
         with gr.Row():
+            # 创建一列用于放置文本输入框和滑块
             with gr.Column():
+                # 正面文本提示输入框
                 prompt = gr.Textbox(interactive=True, label="prompt")
+                # 负面文本提示输入框
                 neg_prompt = gr.Textbox(interactive=True, value="Blurred, incomplete, distorted, misplaced", label="negative prompt")
+                # 指导比例滑块
                 guidance_scale = gr.Slider(0, 20, value=7, label="guidance scale", step=0.5)
+            # 创建另一列用于放置方向控制按钮
             with gr.Column():
                 with gr.Row():
+                    # 方向控制按钮
                     left = gr.Button("LEFT")
                     with gr.Column():
                         up = gr.Button("UP")
                         down = gr.Button("DOWN")
                     right = gr.Button("right")
 
+                    # 设置方向按钮的点击事件
                     left.click(event_process.left_ext, inputs=[image], outputs=[img_window])
                     right.click(event_process.right_ext, inputs=[image], outputs=[img_window])
                     up.click(event_process.up_ext, inputs=[image], outputs=[img_window])
                     down.click(event_process.down_ext, inputs=[image], outputs=[img_window])
+                # 网格大小数字输入框
                 grid_size = gr.Number(128, label="grid size")
+                # 设置网格大小改变时的回调函数
                 grid_size.change(event_process.set_grid_size, inputs=[grid_size])
+        
+        # 创建一行用于放置处理和应用按钮
         with gr.Row():
             with gr.Row():
+                # 处理按钮，点击时调用start_process函数处理图像
                 process = gr.Button("start")
+                # 应用按钮，点击时将处理后的图像应用到原图像上传组件
                 apply = gr.Button("apply")
+            # 采样步骤滑块
             sample_step = gr.Slider(0, 100, value=30, label="sample step", step=1)
+            # 设置处理按钮的点击事件
             process.click(event_process.start_process, inputs=[image, prompt, neg_prompt, sample_step, guidance_scale], outputs=[pre_image])
+            # 设置应用按钮的点击事件
             apply.click(event_process.apply_to, inputs=[pre_image], outputs=[image])
 
 
